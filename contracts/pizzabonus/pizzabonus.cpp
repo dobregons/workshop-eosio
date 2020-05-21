bd34db7771734b3d1e20b86503c976298cd0d229
David Obregon <david.obregon-ext@block.one>
2020-05-21 19:49:59 -0400

    Implementation of buy pizza method

--- contracts/pizzabonus/pizzabonus.cpp
+++ contracts/pizzabonus/pizzabonus.cpp
@@ -1,13 +1,37 @@
 #include <eosio/eosio.hpp>
 #include "./pizzabonus.h"
 
-pizzabonus::pizzabonus(name self_contract, name user, eosio::datastream<const char*> ds) 
-: contract(self_contract, user, ds), ut(""_n, ""_n.value) {
+pizzabonus::pizzabonus(name self_contract, name first_receiver, eosio::datastream<const char*> ds) 
+: contract(self_contract, first_receiver, ds), ut(self_contract, self_contract.value) {
+  // TODO: Research passing custom parameters to the contract constructor
   coupon_threshold = 5;
+  eosio::print("self_contract " , self_contract);
+  eosio::print("first_receiver ", first_receiver);
+  // self_contract pizzabonus = owner of the action
+  // first_receiver pizzabonus ???
 }
 
 void pizzabonus::buypizza(name user) {
-  //
+  // Valid if the user exist in the chain
+  eosio::require_auth(user);
+  auto iterator = ut.find(user.value);
+  if (iterator == ut.end()) { // new user
+    eosio::print("register new user ", user);
+    ut.emplace(user, [&](auto& row) {
+      row.username = user;
+      row.pzsbought = 1;
+    });
+  } else {
+    eosio::print("user already exist and bought a pizza ", user);
+    ut.modify(iterator, user, [&](auto& row) {
+      row.pzsbought++;
+      if(row.pzsbought == coupon_threshold) {
+        row.coupons++;
+        row.pzsbought = 0;
+      }
+      eosio::print("Counter ", row.pzsbought);
+    });
+  }
 }
 
 uint16_t pizzabonus::getcoupons(name user) {
--- contracts/pizzabonus/tables.h
+++ contracts/pizzabonus/tables.h
@@ -2,21 +2,13 @@
 
 #include <eosio/eosio.hpp>
 
-struct [[eosio::table]] coupon {
-  uint16_t value;
-  bool active = false;
-};
-
-// EOSIO_REFLECT(Coupon, value, active);
-
-struct [[eosio::table]] user {
+// table to store user information
+struct [[eosio::table]] pizzauser {
   eosio::name username = ""_n;
-  coupon cpn = coupon{};
+  uint16_t pzsbought = 0;
+  uint16_t coupons = 0;
 
-  // eosio::name primary_key() const { return username; }
   uint64_t primary_key() const { return username.value; }
 };
 
-// EOSIO_REFLECT(User, username, cpm);
-
-typedef eosio::multi_index<"user"_n, user> usertable_t;
+typedef eosio::multi_index<"pizzauser"_n, pizzauser> usertable_t;
